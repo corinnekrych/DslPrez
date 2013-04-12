d09faf4681b1f9aa8979d577eb8806afe6f86ee5
corinnekrych <corinnekrych@gmail.com>
2013-04-12 11:00:33 +0200

    adding music to striptease

--- web-app/js/prez.js
+++ web-app/js/prez.js
@@ -67,18 +67,120 @@ function submitForm(input, output) {
 
 editor0 = new dslPrez.editor("editor0");
 function editor0Key1() {
-    var value = "def ask(question) {\n"
-        + "    println \"question: $question\"\n"
-        + "}\n"
-        + "ask \"what is your name?\"\n";
-    editor0.replaceRange(value, {line:5, ch:0});
+    var value = "class Person {\n" +
+    "    def name\n" +
+    "    def age\n" +
+    "    String toString() {\n" +
+    "        \"Hello I'm \" + name + \" and I'm \" + age + \" years old\"\n " +
+    "    }\n" +
+    "}\n" +
+    "Person sebastien = new Person()\n" +
+    "sebastien.name = \"Sebi\" \n" +
+    "sebastien.age = 34\n" +
+    "println sebastien";
+    for (var i = 0; i < 32; i++) {
+        editor0.removeLine(0);
+    }
+    editor0.replaceRange(value, {line:0, ch:0});
 }
+
+//var ytplayer = null;
+//
+//function onYouTubePlayerReady(playerId) {
+//    ytplayer = document.getElementById("ytplayer");
+//}
+//
+//function onPlay() {
+//    if (ytplayer) {
+//        ytplayer.playVideo();
+//    }
+//}
+
+function callPlayer(frame_id, func, args) {
+    if (window.jQuery && frame_id instanceof jQuery) frame_id = frame_id.get(0).id;
+    var iframe = document.getElementById(frame_id);
+    if (iframe && iframe.tagName.toUpperCase() != 'IFRAME') {
+        iframe = iframe.getElementsByTagName('iframe')[0];
+    }
+
+    // When the player is not ready yet, add the event to a queue
+    // Each frame_id is associated with an own queue.
+    // Each queue has three possible states:
+    //  undefined = uninitialised / array = queue / 0 = ready
+    if (!callPlayer.queue) callPlayer.queue = {};
+    var queue = callPlayer.queue[frame_id],
+        domReady = document.readyState == 'complete';
+
+    if (domReady && !iframe) {
+        // DOM is ready and iframe does not exist. Log a message
+        window.console && console.log('callPlayer: Frame not found; id=' + frame_id);
+        if (queue) clearInterval(queue.poller);
+    } else if (func === 'listening') {
+        // Sending the "listener" message to the frame, to request status updates
+        if (iframe && iframe.contentWindow) {
+            func = '{"event":"listening","id":' + JSON.stringify(''+frame_id) + '}';
+            iframe.contentWindow.postMessage(func, '*');
+        }
+    } else if (!domReady || iframe && (!iframe.contentWindow || queue && !queue.ready)) {
+        if (!queue) queue = callPlayer.queue[frame_id] = [];
+        queue.push([func, args]);
+        if (!('poller' in queue)) {
+            // keep polling until the document and frame is ready
+            queue.poller = setInterval(function() {
+                callPlayer(frame_id, 'listening');
+            }, 250);
+            // Add a global "message" event listener, to catch status updates:
+            messageEvent(1, function runOnceReady(e) {
+                var tmp = JSON.parse(e.data);
+                if (tmp && tmp.id == frame_id && tmp.event == 'onReady') {
+                    // YT Player says that they're ready, so mark the player as ready
+                    clearInterval(queue.poller);
+                    queue.ready = true;
+                    messageEvent(0, runOnceReady);
+                    // .. and release the queue:
+                    while (tmp = queue.shift()) {
+                        callPlayer(frame_id, tmp[0], tmp[1]);
+                    }
+                }
+            }, false);
+        }
+    } else if (iframe && iframe.contentWindow) {
+        // When a function is supplied, just call it (like "onYouTubePlayerReady")
+        if (func.call) return func();
+        // Frame exists, send message
+        iframe.contentWindow.postMessage(JSON.stringify({
+            "event": "command",
+            "func": func,
+            "args": args || [],
+            "id": frame_id
+        }), "*");
+    }
+    /* IE8 does not support addEventListener... */
+    function messageEvent(add, listener) {
+        var w3 = add ? window.addEventListener : window.removeEventListener;
+        w3 ?
+            w3('message', listener, !1)
+            :
+            (add ? window.attachEvent : window.detachEvent)('onmessage', listener);
+    }
+}
+
+function onPlay() {
+    callPlayer('whateverID', 'playVideo');
+}
+
+function onStop() {
+    callPlayer('whateverID', 'stopVideo');
+}
+
 function editor0Send() {
     var value = editor0.getValue();
     submitForm(value, "#output0");
 }
 var keymap = {
+    "0" : onPlay,
     "1" : editor0Key1,
+    "2" : onStop,
     "Ctrl-S" : editor0Send,
     "Cmd-S" : editor0Send
 };
@@ -108,6 +210,7 @@ editor1.addKeyMap(keymap);
 
 editor2 = new dslPrez.editor("editor2");
 function editor2Key1() {
+
     var value = "abstract class SurveyScript extends Script {\n"
         + "  def ask = {question -> println \"question: $question\" }\n"
         + "}\n";
