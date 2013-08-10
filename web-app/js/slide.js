dd6fcef214e59e1c37cfb12cde279ad790873608
fabricematrat <fabricematrat@gmail.com>
2013-08-11 01:20:52 +0200

    add progress bar

--- web-app/js/slide.js
+++ web-app/js/slide.js
@@ -1,33 +1,42 @@
-var slides = $('.slide');
-slides.each(function(index) {
-    if (index != 0) {
-        $(this).hide();
-    } else {
-        $(this).addClass('showSlide');
-        $(this).attr('indexSlide', index);
-    }
-});
+var dslPrez = dslPrez || {};
 
-$(document).keydown(function(e){
-    if (!inEditor) {
-        var value;
-        switch(e.which) {
-            case 37:
-                value = 0;
-                break;
-            case 39:
-                value = 1;
-                break;
-            default: return; // allow other keys to be handled
+dslPrez.Slide = function () {
+    var that = this;
+    var slides = $('.slide');
+    var callEnter = {};
+
+    slides.each(function(index) {
+        if (index != 0) {
+            $(this).hide();
+        } else {
+            $(this).addClass('showSlide');
+            $(this).attr('indexSlide', index);
         }
-        changeSlide(e, value);
-    }
-});
+    });
 
-var changeSlide = function(e, value) {
+    $(document).keydown(function(e){
+        if (!inEditor) {
+            var value;
+            switch(e.which) {
+                case 37:
+                    value = 0;
+                    break;
+                case 39:
+                    value = 1;
+                    break;
+                default: return; // allow other keys to be handled
+            }
+            changeSlide(e, value);
+        }
+    });
+
+    var currentSlideIndex = function() {
         var selectedSlide = $('.showSlide')[0];
-        var indexSlide = parseInt($(selectedSlide).attr('indexSlide'));
+        return parseInt($(selectedSlide).attr('indexSlide'));
+    };
 
+    var changeSlide = function(e, value) {
+        var indexSlide = currentSlideIndex();
         switch(value) {
             case 0:
                 hideSlide(indexSlide);
@@ -55,134 +64,130 @@ var changeSlide = function(e, value) {
         if (e != null) {
             e.preventDefault();
         }
-}
-
-var startSlide = function(index) {
-    if (index > slides.length-1) {
-        index = 0;
     }
-    hideSlide(0);
-    showSlide(index);
-}
 
-$('#run').click(function(e) {
+    var startSlide = function(index) {
+        if (index > slides.length-1) {
+            index = 0;
+        }
+        hideSlide(0);
+        showSlide(index);
+    }
 
-    var url = serverUrl + "/survey/runTurtle?=";
-    $.post(url, {   }, function(data) {
-        var toto =data;
-        if(data.finished==true) {
-            $(".surveystart").hide();
-            for(var index = 1; index <= counter;index++)  {
-                if (answerMap[index]) {
-                    var output8Value = '<div class="displayAnswer">' + answerMap[index].question + ' ' + answerMap[index].answer + '</div>';
-                    $("#output8bis").append(output8Value);
+    $('#previous').click(function(e) {
+        changeSlide(e, 0);
+    });
+
+    $('#next').click(function(e) {
+        changeSlide(e, 1);
+    });
+
+    var hideSlide = function(index) {
+        $(slides[index]).removeClass('showSlide');
+        $(slides[index]).removeAttr('indexSlide');
+        $(slides[index]).slideUp(600);
+    };
+
+    var showSlide = function(index) {
+        $('#count').empty().append("  " + index + "/" + (slides.length-1) + "  ");
+        window.history.pushState("", "Title", "/DslPrez/#" + index);
+
+        $(slides[index]).addClass('showSlide');
+        $(slides[index]).attr('indexSlide', index);
+        $(slides[index]).slideDown(600, function(){
+            if (callEnter[$(slides[index]).attr('title')] != null) {
+                callEnter[$(slides[index]).attr('title')]();
+            } else if ($(slides[index]).children()[0].type === 'textarea') {
+                window[$(slides[index]).children()[0].id].refresh();
+                window[$(slides[index]).children()[0].id].focus();
+            }
+        });
+        var previous;
+        if (index === 0) {
+            previous = slides.size()-1;
+        } else {
+            previous = index -1;
+        }
+        var next;
+        if (index === slides.size()) {
+            next = 0;
+        } else {
+            next = index + 1;
+        }
+
+        var newPrevious = $(slides[previous]).attr('title');
+        $('#previous-title').empty().append(newPrevious);
+        var newNext = $(slides[next]).attr('title');
+        $('#next-title').empty().append(newNext);
+        $('#currentTitle').empty().append($(slides[index]).attr('title'));
+        $('#currentPress').empty().append($(slides[index]).attr('press'));
+    };
+
+    that.enter = function(title, callback) {
+        callEnter[title] = callback;
+    };
+
+    var startProgress  = function (conf){
+        if (conf.progressbar) {
+
+            var div = $('<div>');
+            div.attr({
+                id: 'container',
+                class: 'progress-container'
+            });
+            var progress = $('<div>');
+            progress.attr({
+                id: 'progress-bar',
+                class: 'progress'
+            });
+            div.append(progress);
+
+            div.insertAfter($('#currentPress'));
+            var startDate = new Date().getTime();
+            var totalDuration = conf.duration*1000;
+
+            var progress;
+            function clock() {
+                var $s = $("#progress-bar");
+                var d = new Date().getTime();
+                var diff = d - startDate;
+                var percent = (diff*100)/totalDuration;
+                if (percent >100) {
+                    window.clearInterval(progress);
+                    percent = 100;
+                }
+                $s.width(percent + "%").trigger("change");
+
+                var idealTime = $(slides[currentSlideIndex()]).attr('timing');
+                if (idealTime) {
+                    if (Math.abs(idealTime - diff) < (totalDuration*conf.progressPrecision)/100) {
+                        $s.css("background", "green");
+                    } else if (idealTime > diff){
+                        $s.css("background", "yellow");
+                    } else {
+                        $s.css("background", "red");
+                    }
                 }
             }
-            $('#next').click();
+            progress = window.setInterval(clock, 1000);
         }
-    });
-});
+    };
 
-$('#previous').click(function(e) {
-    changeSlide(e, 0);
-});
+    that.start = function(conf) {
+        var url = window.location.href;
 
-$('#next').click(function(e) {
-    changeSlide(e, 1);
-});
-
-var hideSlide = function(index) {
-    $(slides[index]).removeClass('showSlide');
-    $(slides[index]).removeAttr('indexSlide');
-    $(slides[index]).slideUp(600);
-};
-
-var myFunction = function() {
-  alert("button pressed");
-};
-
-var showSlide = function(index) {
-    $('#count').empty().append(index + "/" + (slides.length-1) + "  ");
-    window.history.pushState("", "Title", "/DslPrez/#" + index);
-
-    $(slides[index]).addClass('showSlide');
-    $(slides[index]).attr('indexSlide', index);
-    $(slides[index]).slideDown(600, function(){
-        $('#key1').remove();
-        $('#key2').remove();
-        $('#key3').remove();
-        $('#key4').remove();
-        $('#key5').remove();
-        $('#key6').remove();
-        $('#send').remove();
-        if ($(slides[index]).attr('title') === 'Groovy') {
-            showCloud();
-        } else if ($(slides[index]).children()[0].type === 'textarea') {
-            window[$(slides[index]).children()[0].id].refresh();
-            window[$(slides[index]).children()[0].id].focus();
+        var pos = url.indexOf('#');
+        if (pos < 0) {
+            var val = 0;
         }
-    });
-    var previous;
-    if (index === 0) {
-        previous = slides.size()-1;
-    } else {
-        previous = index -1;
-    }
-    var next;
-    if (index === slides.size()) {
-        next = 0;
-    } else {
-        next = index + 1;
-    }
+        else {
+            var val = Number(url.substring(pos + 1));
+        }
+        startSlide(val);
+        startProgress(conf);
+    };
 
-    var newPrevious = $(slides[previous]).attr('title');
-    $('#previous-title').empty().append(newPrevious);
-    var newNext = $(slides[next]).attr('title');
-    $('#next-title').empty().append(newNext);
-    $('#currentTitle').empty().append($(slides[index]).attr('title'));
-    $('#currentPress').empty().append($(slides[index]).attr('press'));
-};
-
-var showCloud = function() {
-    var word_array = [
-        {text: "syntax sugar", weight: 9},
-        {text: "less punctuation", weight: 7},
-        {text: "scripting", weight: 5},
-        {text: "binding", weight: 7},
-        {text: "no getter setter", weight: 1},
-        {text: "map", weight: 7},
-        {text: "named parameters", weight: 8},
-        {text: "closure", weight: 6},
-        {text: "GString", weight: 4},
-        {text: "public visibility", weight: 2},
-        {text: "return statement optional", weight: 3},
-        {text: "groovy truth", weight: 7},
-        {text: "operator overloading", weight: 1},
-        {text: "GroovyMarkup", weight: 4},
-        {text: "method pointer", weight: 6},
-        {text: "runtime transformation", weight: 1},
-        {text: "MOP", weight: 9},
-        {text: "Expando", weight: 8},
-        {text: "Categories", weight: 7},
-        {text: "methodMissing", weight: 8},
-        {text: "command chaining", weight: 1},
-        {text: "compile time transformation", weight: 7},
-        {text: "AST transformation", weight: 10},
-        {text: "CST transformation", weight: 5},
-        {text: "TypeChecked DSL", weight: 10}
-    ];
-    $("#wordcloud").empty();
-    $("#wordcloud").jQCloud(word_array);
+    return that;
 };
 
 
-var url = window.location.href;
-
-var pos = url.indexOf('#');
-if (pos < 0) {
-    var val = 0;
-}
-else {
-    var val = Number(url.substring(pos + 1));
-}
-startSlide(val);
